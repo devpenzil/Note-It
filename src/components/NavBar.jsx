bfba332d43632e77ee832f78a1f5c7707f6d42ba
ajoalex012 <ajoalex012@gmail.com>
2021-09-20 22:12:50 +0530

    Finish : NavBar Functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,25 @@ import { useState } from 'react'
 import './NavBar.scss'
 function NavBar() {
     const [shownav, setShownav] = useState(false)
+    const [notetitle, setNotetitle] = useState("")
+    const [notenote, setNotenote] = useState("")
+    if (localStorage.length === 0) {
+        localStorage.setItem("Notes", JSON.stringify([{}]))
+    }
+    const addnote = () => {
+        if (notetitle == "" || notenote == "") {
+            alert("Please fill all the fields")
+            return 0
+        }
+        const note = {title : notetitle , notecontent : notenote}
+        let accessnotes = JSON.parse(localStorage.getItem("Notes"))
+        accessnotes = accessnotes.concat(note)
+        localStorage.setItem("Notes", JSON.stringify(accessnotes))
+        setShownav(false)
+        setNotenote("")
+        setNotetitle("")
+        alert("Note successfully created")
+    }
     return (
         <div className="NavBar container">
             <div className="nav">
@@ -17,12 +36,13 @@ function NavBar() {
             <div className="create-note">
                 <div className="content">
                     <div className="heading">Create a Note</div>
+                   
                     <div className="form">
-                        <input placeholder="Enter your title here" className="note-title" type="text" name="title" id="title" />
-                        <textarea placeholder="Enter Your Note here" className="note-note" name="note" id="note" cols="30" rows="15"></textarea>
+                        <input onChange={ (e) => setNotetitle(e.target.value)} placeholder="Enter your title here" className="note-title" type="text" name="title" id="title" />
+                        <textarea onChange={ (e) => setNotenote(e.target.value)} placeholder="Enter Your Note here" className="note-note" name="note" id="note" cols="30" rows="15"></textarea>
                     </div>
                     <div className="ctas">
-                        <button>Add to notes</button>
+                        <button onClick={addnote}>Add to notes</button>
                         <button className="close-button" onClick={ () => setShownav(false) }>Close</button>  
                     </div>   
                 </div>
