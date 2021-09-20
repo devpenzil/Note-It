0d57ba2542f27e598960843f156986b80febb968
ajoalex012 <ajoalex012@gmail.com>
2021-09-20 21:20:26 +0530

    Finish : NavBar ui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 import './NavBar.scss'
 function NavBar() {
+    const [shownav, setShownav] = useState(false)
     return (
         <div className="NavBar container">
             <div className="nav">
@@ -8,9 +10,25 @@ function NavBar() {
                     logo
                 </div>
                 <div>
-                    <button className="create">Create a new note</button>
+                    <button onClick={()=>setShownav(true)} className="create">🖋 Create a new note</button>
                 </div>
             </div>
+            {shownav ?
+            <div className="create-note">
+                <div className="content">
+                    <div className="heading">Create a Note</div>
+                    <div className="form">
+                        <input placeholder="Enter your title here" className="note-title" type="text" name="title" id="title" />
+                        <textarea placeholder="Enter Your Note here" className="note-note" name="note" id="note" cols="30" rows="15"></textarea>
+                    </div>
+                    <div className="ctas">
+                        <button>Add to notes</button>
+                        <button className="close-button" onClick={ () => setShownav(false) }>Close</button>  
+                    </div>   
+                </div>
+                    
+            </div>
+            : ""}
         </div>
     )
 }
